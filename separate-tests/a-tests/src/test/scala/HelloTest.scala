e55f28df759e450d7dfe6716c48230cec80d81f8
Stu <maiflai@nospam>
2015-01-25 12:54:36 +0000

    add an example with tests held in a separate project

---
+++ separate-tests/a-tests/src/test/scala/HelloTest.scala
@@ -0,0 +1,10 @@
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class HelloTest {
+
+  @Test def testText() {
+    assertEquals("Hello World", new Hello().text)
+  }
+
+}
---
+++ separate-tests/a/src/main/scala/Hello.scala
@@ -0,0 +1,3 @@
+class Hello {
+  def text = "Hello World"
+}
