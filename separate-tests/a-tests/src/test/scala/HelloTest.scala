b74169d53431377773372423c4c70db920f479c2
Stu <maiflai@nospam>
2015-12-17 16:50:23 +0000

    a-tests should have common test code under src/main/scala

--- separate-tests/a-tests/src/test/scala/HelloTest.scala
+++ separate-tests/a-tests/src/test/scala/HelloTest.scala
@@ -1,7 +1,7 @@
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
-class HelloTest {
+class HelloTest extends BaseTest {
 
   @Test def testText() {
     assertEquals("Hello World", new Hello().text)
